d7a69c2cfa76b3b01b10d06f4b49266ead0a25fb
Luke van Aardt <lukev@bbd.co.za>
2021-06-07 12:43:03 +0200

    port test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { performMonitoring } = require('./utils/monitor')
 
 const app = express();
 
-const PORT = process.env.PORT ?? 443;
+const PORT = process.env.PORT ?? 3000;
 
 app.get('/', async (req, res) => {
     console.log('Request received');
